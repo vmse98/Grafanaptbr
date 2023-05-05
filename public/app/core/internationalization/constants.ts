1f5528510b4f2e091fa9aeae588b2a86f9a350e0
vmse98 <125083996+vmse98@users.noreply.github.com>
2023-05-05 09:32:31 -0300

    Tradução realizada

--- public/app/core/internationalization/constants.ts
+++ public/app/core/internationalization/constants.ts
@@ -1,6 +1,7 @@
 import { ResourceKey } from 'i18next';
 
 export const ENGLISH_US = 'en-US';
+export const PORTUGUESE_BR = 'pt-BR';
 export const FRENCH_FRANCE = 'fr-FR';
 export const SPANISH_SPAIN = 'es-ES';
 export const GERMAN_GERMANY = 'de-DE';
@@ -31,6 +32,12 @@ export const LANGUAGES: LanguageDefinitions[] = [
     code: FRENCH_FRANCE,
     name: 'Français',
     loader: () => import('../../../locales/fr-FR/grafana.json'),
+  },
+
+  {
+    code: PORTUGUESE_BR,
+    name: 'Portugues',
+    loader: () => import('../../../locales/pt-BR/grafana.json'),
   },
 
   {
